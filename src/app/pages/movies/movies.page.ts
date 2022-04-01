9fa2c840f031a35dbb6930e3a4150899507745f3
sone <sthaya@gmail.com>
2022-03-31 17:13:52 -0700

    started movie app

---
+++ src/app/pages/movies/movies.page.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { MovieService } from 'src/app/services/movie.service';
+
+@Component({
+  selector: 'app-movies',
+  templateUrl: './movies.page.html',
+  styleUrls: ['./movies.page.scss'],
+})
+export class MoviesPage implements OnInit {
+  constructor(private movieService: MovieService) {}
+
+  ngOnInit() {}
+}
